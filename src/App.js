c22cf787f6971f2f07e8405593dc83906cd166d3
TerryThreatt <terry.threatt@gmail.com>
2021-09-19 13:36:57 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import Astronomy from './components/Astronomy';
 
 function App() {
+  // api state
+  const [data, setData] = useState([])
+  const API_KEY = process.env.REACT_APP_KEY
+  const today = new Date()
+  const lastweek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}?count=20?start_date=${lastweek}`
+
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(res => setData(res))
+      .catch(error => console.log(error))
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Spacestagram</h1>
       </header>
+      <div className="astronomy">
+        <Astronomy data={data}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Astronomy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Astronomy() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Astronomy
